a3999614c77eac13fe437b90a92ef3b4b09d3eea
bogdan <bogy2015@mail.ru>
2024-03-18 21:28:39 +0400

    dz

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,41 @@
-function stringCheck (str, maxStringLength) {
-  if (str.length <= maxStringLength) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-stringCheck('hello', 4);
-
-function isPalindrome (str) {
-  const normalStr = (str.replaceAll(' ', '')).toUpperCase();
-  let emptyStr = '';
-
-  for (let i = normalStr.length - 1; i >= 0; i--) {
-    emptyStr += normalStr[i];
-  }
-
-  return emptyStr === normalStr;
-}
-
-isPalindrome('Леша на полке клопа нашел ');
-
-//Доп.дз
-// function stringToNumber (str) {
-//   for(let i = 0; i < str.length; i++) {
-//     const num = parseInt(str[i], 10);
-//     console.log(num);
+// function stringCheck (str, maxStringLength) {
+//   if (str.length <= maxStringLength) {
+//     return true;
+//   } else {
+//     return false;
 //   }
 // }
 
-// stringToNumber ('agent007');
+// stringCheck('hello', 4);
+
+// function isPalindrome (str) {
+//   const normalStr = (str.replaceAll(' ', '')).toUpperCase();
+//   let emptyStr = '';
+
+//   for (let i = normalStr.length - 1; i >= 0; i--) {
+//     emptyStr += normalStr[i];
+//   }
+
+//   return emptyStr === normalStr;
+// }
+
+// isPalindrome('Леша на полке клопа нашел ');
+
+const convertTimeToNumber = (time) => {
+  const [hours, minutes] = time.split(":");
+
+  const hoursInt = parseInt(hours, 10);
+  const minutesInt = parseInt(minutes, 10);
+
+  return hoursInt * 60 + minutesInt;
+};
+
+const IsMeetingInWorkTime = (workStart, workEnd, meetingStart, duration) => {
+  workStart = convertTimeToNumber(workStart);
+  workEnd = convertTimeToNumber(workEnd);
+  meetingStart = convertTimeToNumber(meetingStart);
+
+  return meetingStart + duration <= workEnd && meetingStart >= workStart;
+};
+
+// console.log(IsMeetingInWorkTime("14:00", "17:30", "08:0", 90));
